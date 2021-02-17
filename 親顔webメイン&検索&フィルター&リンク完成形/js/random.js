f331bc3915c042a38dd0a404adb2ee25fdcd68fa
TRPGmiru <78581352+TRPGmiru@users.noreply.github.com>
2021-02-17 22:04:15 +0900

    Add files via upload

---
+++ 親顔webメイン&検索&フィルター&リンク完成形/js/random.js
@@ -0,0 +1,45 @@
+const movieAreaDizm = document.getElementById('movieAreaDizm');
+const moviesDizm = ['https://www.youtube.com/embed/uBfcrqm-NL0', 'https://www.youtube.com/embed/8KoO2s4DLo4', 'https://www.youtube.com/embed/HoqBA1kC_Eo', 'https://www.youtube.com/embed/YLbzl0voOUI', 'https://www.youtube.com/embed/7WKARj1slPs'];
+
+const movieNoDizm = Math.floor( Math.random() * moviesDizm.length)
+movieAreaDizm.src = moviesDizm[movieNoDizm];
+
+
+
+const movieAreaPapav = document.getElementById('movieAreaPapav');
+const moviesPapav = ['https://www.youtube.com/embed/FRXiuBkl2qQ', 'https://www.youtube.com/embed/bHYNKKllAV4', 'https://www.youtube.com/embed/EcrEWKMfC5E', 'https://www.youtube.com/embed/Bf18uFKDe3U', 'https://www.youtube.com/embed/MAA20IPFRaY'];
+
+const movieNoPapav = Math.floor( Math.random() * moviesPapav.length)
+movieAreaPapav.src = moviesPapav[movieNoPapav];
+
+
+
+const movieAreaMuchu = document.getElementById('movieAreaMuchu');
+const moviesMuchu = ['https://www.youtube.com/embed/4X6fk6jLN9U', 'https://www.youtube.com/embed/IAyvMRCRkFE', 'https://www.youtube.com/embed/zXfhEztvfPE', 'https://www.youtube.com/embed/bZJFX0XAV2A', 'https://www.youtube.com/embed/P6fQgxDMFlE'];
+
+const movieNoMuchu = Math.floor( Math.random() * moviesMuchu.length)
+movieAreaMuchu.src = moviesMuchu[movieNoMuchu];
+
+
+
+const movieAreaTakao = document.getElementById('movieAreaTakao');
+const moviesTakao = ['https://www.youtube.com/embed/OG9lYPND81o', 'https://www.youtube.com/embed/TYlrxV4Bfhs', 'https://www.youtube.com/embed/-aLVG1Vy1GQ', 'https://www.youtube.com/embed/AvlPi11eX5U', 'https://www.youtube.com/embed/4I5rlTkC4Nw'];
+
+const movieNoTakao = Math.floor( Math.random() * moviesTakao.length)
+movieAreaTakao.src = moviesTakao[movieNoTakao];
+
+
+
+const movieAreaTokiyan = document.getElementById('movieAreaTokiyan');
+const moviesTokiyan = ['https://www.youtube.com/embed/xOjASigLyT8', 'https://www.youtube.com/embed/suKFNbROLwo', 'https://www.youtube.com/embed/vYW48CInQSE', 'https://www.youtube.com/embed/zIU-K6RE6dk', 'https://www.youtube.com/embed/CH8sFj6RL5M'];
+
+const movieNoTokiyan = Math.floor( Math.random() * moviesTokiyan.length)
+movieAreaTokiyan.src = moviesTokiyan[movieNoTokiyan];
+
+
+
+const movieAreaUsi = document.getElementById('movieAreaUsi');
+const moviesUsi = ['https://www.youtube.com/embed/AUi4v1HRBg0', 'https://www.youtube.com/embed/8hPVM7d_3h0', 'https://www.youtube.com/embed/3lY9Rl8iLTQ', 'https://www.youtube.com/embed/CcHiUchytWg', 'https://www.youtube.com/embed/oEcK3xyN2oc'];
+
+const movieNoUsi = Math.floor( Math.random() * moviesUsi.length)
+movieAreaUsi.src = moviesUsi[movieNoUsi];
